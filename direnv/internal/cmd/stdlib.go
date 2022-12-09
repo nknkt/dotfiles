9070cc8fec03e719a80c8e939e24d11022e8f581
vogaro-kanno <k.kanno@vogaro.co.jp>
2022-12-09 14:15:39 +0900

    remove:node バージョン管理ツールを変更

--- direnv/internal/cmd/stdlib.go
+++
@@ -1,8 +0,0 @@
-package cmd
-
-import "strings"
-
-// getStdlib returns the stdlib.sh, with references to direnv replaced.
-func getStdlib(config *Config) string {
-	return strings.Replace(stdlib, "$(command -v direnv)", config.SelfPath, 1)
-}
--- direnv/test/direnv-test.zsh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env zsh
-export TARGET_SHELL=zsh
-source "$(dirname $0)/direnv-test-common.sh"
