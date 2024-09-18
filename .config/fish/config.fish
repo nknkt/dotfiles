64650f2f65ac1ed0c8c1c289fd8e00b2b7ae99eb
vogaro-kanno <k.kanno@vogaro.co.jp>
2024-09-18 12:41:57 +0900

    [update]pecoのキーバインド変更

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,9 +9,9 @@ set -g GHQ_SELECTOR peco
 
 function fish_user_key_bindings
 	bind \cc 'peco_cd'
-	bind \cx\ck peco_kill
+	bind \ck peco_kill
 	bind \cw 'peco_select_history (commandline -b)'
-	bind \cx\cr peco_recentd
+	bind \cr peco_recentd
 end
 
 starship init fish | source
