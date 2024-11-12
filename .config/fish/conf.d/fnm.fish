14cf320ca0cc5ec781a1e7ac23f43513c5b3448a
vogaro-kanno <k.kanno@vogaro.co.jp>
2024-11-12 17:18:35 +0900

    [fix]fnmバージョンアップ パスを自動に、fnm envコマンドを通し、自動切り替えを許可

--- .config/fish/conf.d/fnm.fish
+++ .config/fish/conf.d/fnm.fish
@@ -1,1 +1,8 @@
 fnm env --use-on-cd | source
+
+# fnm
+set FNM_PATH "/Users/kenta_kanno/Library/Application Support/fnm"
+if [ -d "$FNM_PATH" ]
+  set PATH "$FNM_PATH" $PATH
+  fnm env | source
+end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
+# homebrew
 set PATH /opt/homebrew/bin $PATH
 
 # Golang
@@ -6,22 +7,21 @@ set -x PATH $PATH $GOPATH/bin
 
 # ghq + peco
 set -g GHQ_SELECTOR peco
-
 function fish_user_key_bindings
-	bind \cc 'peco_cd'
-	bind \ck peco_kill
-	bind \cw 'peco_select_history (commandline -b)'
-	bind \cr peco_recentd
+    bind \cc 'peco_cd'
+    bind \ck peco_kill
+    bind \cw 'peco_select_history (commandline -b)'
+    bind \cr peco_recentd
 end
 
+# starship
 starship init fish | source
 
 # fnm
-set -gx PATH "/Users/kenta_kanno/Library/Caches/fnm_multishells/80456_1671090326072/bin" $PATH;
-set -gx FNM_VERSION_FILE_STRATEGY "local";
-set -gx FNM_LOGLEVEL "info";
-set -gx FNM_NODE_DIST_MIRROR "https://nodejs.org/dist";
-set -gx FNM_ARCH "arm64";
-set -gx FNM_DIR "/Users/kenta_kanno/Library/Application Support/fnm";
-set -gx FNM_MULTISHELL_PATH "/Users/kenta_kanno/Library/Caches/fnm_multishells/80456_1671090326072";
+eval (fnm env)
+
+# Provides the ability to automatically switch Node.js versions when a directory is changed
+fnm env --use-on-cd --shell fish | source
+
+# docker
 source /Users/kenta_kanno/.docker/init-fish.sh || true # Added by Docker Desktop
