85c5bfaaa3db42ebad58dcc43c82e200accc7246
vogaro-kanno <k.kanno@vogaro.co.jp>
2025-10-20 16:24:06 +0900

    Improve fish shell functions and completions
    - インデントの統一（スペース4つ）
    - コードの可読性向上
    - エラーハンドリングの改善
    - 文字列処理の最適化
    - peco_cd: エラーハンドリングとUIの改善
    - z関数群: プライベートモード対応とバグ修正

--- .config/fish/functions/peco_cd.fish
+++ .config/fish/functions/peco_cd.fish
@@ -1,7 +1,12 @@
-    function peco_cd
-      set selected_dir (ls -1d */ | peco)
-      if [ $selected_dir ]
-        cd $selected_dir
-        commandline -f repaint
-      end
+function peco_cd
+    # 現在のディレクトリにサブディレクトリがある場合
+    if count (ls -1d */ 2>/dev/null) >/dev/null
+        set selected_dir (ls -1d */ | peco --prompt="CD> ")
+        if test -n "$selected_dir"
+            cd $selected_dir
+            commandline -f repaint
+        end
+    else
+        echo "No subdirectories found in current directory."
     end
+end
